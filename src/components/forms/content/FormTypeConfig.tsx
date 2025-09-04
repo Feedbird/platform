4dc38b9c80329da23a107d204335240357511985
Mateo Guerrero <mateof1223@gmail.com>
2025-09-03 22:07:56 -0500

    Set up initial field configurations

--- src/components/forms/content/FormTypeConfig.tsx
+++ src/components/forms/content/FormTypeConfig.tsx
@@ -1,25 +1,54 @@
 import { UIFormFieldDefaults } from "@/lib/forms/fields";
 import React from "react";
+import FieldConfigWrapper from "./configs/CommonFieldConfig";
+import Image from "next/image";
 
 type Props = {
   isVisible?: boolean;
+  setVisible: React.Dispatch<
+    React.SetStateAction<{ id: string; type: string } | null>
+  >;
   type: string;
 };
 
-export default function FormTypeConfig({ isVisible = true, type }: Props) {
-  const fieldConfig =
+export default function FormTypeConfig({
+  isVisible = true,
+  type,
+  setVisible,
+}: Props) {
+  const defaultFieldDefinition =
     UIFormFieldDefaults[type as keyof typeof UIFormFieldDefaults] || null;
 
-  console.log(JSON.stringify(fieldConfig, null, 2));
+  // Always render the container for smooth animations, but conditionally render content
   return (
     <div
       className={`absolute right-0 border-border-primary z-20 border-l-1 w-[320px] bg-[#FAFAFA] h-full flex-shrink-0 flex flex-col transition-transform duration-300 ease-in-out ${
         isVisible ? "transform translate-x-0" : "transform translate-x-full"
       }`}
     >
-      <header className="border-border-primary border-b-1 w-full p-3 text-black font-medium">
-        Edit Field
-      </header>
+      {defaultFieldDefinition && (
+        <>
+          <header className="border-border-primary border-b-1 w-full p-3 text-black font-medium flex gap-2">
+            <Image
+              src="/images/boards/arrow-left.svg"
+              alt="back_icon"
+              width={15}
+              onClick={() => setVisible(null)}
+              className="cursor-pointer"
+              height={15}
+            />
+            <div className="flex flex-row gap-1">
+              <span>Edit Field</span>
+              <p className="text-[#838488] font-normal">
+                ({Object.keys(defaultFieldDefinition.config).length})
+              </p>
+            </div>
+          </header>
+          <div>
+            <FieldConfigWrapper field={defaultFieldDefinition} />
+          </div>
+        </>
+      )}
     </div>
   );
 }
