84ec55a8e00d9e4ac9e2cbca7ad069fcb0e64476
Together <144047004+together320@users.noreply.github.com>
2025-09-22 06:15:56 -0800

    - Modify the middleware

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -41,7 +41,7 @@ const inter = Inter({
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}>
       <html lang="en" suppressHydrationWarning>
         <head>
           <link rel="icon" type="image/svg+xml" href="/images/logo/logo.svg" />
